07e465e4db2bf540297ef56ef29dd4bc89f95264
Andrew F Ardire <aardire@gmail.com>
2017-08-16 19:27:54 -0400

    Udactiy Client Working Properly

--- OnTheMap/UdactiyClient.swift
+++ OnTheMap/UdactiyClient.swift
@@ -16,6 +16,7 @@ class UdactiyClient : NSObject {
     
     // shared session
     var session = URLSession.shared
+    var userKey: String?
     
     
     // MARK: Convenience Functions
@@ -31,27 +32,23 @@ class UdactiyClient : NSObject {
             }
             
             guard let registration = accountInfo["registered"] as? Bool else {
-                completionHandlerForUserID(false,nil,"Error Retrieving Account Information")
+                completionHandlerForUserID(false,nil,"Error Retrieving Registration Information")
                 return
             }
             
             guard let userKey = accountInfo["key"] as? String else {
-                completionHandlerForUserID(false,nil,"Error Retrieving Account Information")
+                completionHandlerForUserID(false,nil,"Error Retrieving Key Information")
                 return
             }
             
-            
+            self.userKey = userKey
             completionHandlerForUserID(registration,userKey,nil)
-            
-
         }
-        
-        
     }
     
     // MARK: networking functions
     
-    func taskForUdacityPOST(_ parameters: [String:AnyObject], completionHandlerForPOST: @escaping (_ result: [String:AnyObject]?, _ error: NSError?) -> Void) -> URLSessionDataTask  {
+    func taskForUdacityPOST(_ parameters: [String:AnyObject], completionHandlerForPOST: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> URLSessionDataTask  {
         
        /* 1. Set the parameters */
         let userName = parameters["username"] as? String
@@ -62,7 +59,7 @@ class UdactiyClient : NSObject {
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = "{\"udacity\": {\"username\": \"\(userName)\", \"password\": \"\(userPassword)\"}}".data(using: String.Encoding.utf8)
+        request.httpBody = "{\"udacity\": {\"username\": \"\(userName!)\", \"password\": \"\(userPassword!)\"}}".data(using: String.Encoding.utf8)
         
         /* 4. Make the request */
         let task = session.dataTask(with: request as URLRequest) { data, response, error in
@@ -79,26 +76,46 @@ class UdactiyClient : NSObject {
                 return
             }
           
-            let range = Range(5..<data!.count)
-            let newData = data?.subdata(in: range) /* subset response data! */
-            
-            /* 5/6. Parse the data and use the data */
-            
-            let parsedResult: AnyObject!
-            do {
-                parsedResult = try JSONSerialization.jsonObject(with: newData!, options: .allowFragments) as! [String:AnyObject]
-            } catch {
-                sendError("Could not parse the data as JSON: '\(newData)'")
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("Your request returned a status code other than 2xx!")
                 return
             }
             
-            completionHandlerForPOST(parsedResult,nil)
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            let range = Range(5..<data.count)
+            let newData = data.subdata(in: range) /* subset response data! */
+            
+            /* 5/6. Parse the data and use the data (happens in completion handler) */
+            self.convertDataWithCompletionHandler(newData, completionHandlerForConvertData: completionHandlerForPOST)
+           
 
         }
         
         /* 7. Start the request */
         task.resume()
         return task
+    }
+    
+    // MARK: Helpers
+    
+    // given raw JSON, return a usable Foundation object
+    private func convertDataWithCompletionHandler(_ data: Data, completionHandlerForConvertData: (_ result: AnyObject?, _ error: NSError?) -> Void) {
+        
+        var parsedResult: AnyObject! = nil
+        do {
+            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+        } catch {
+            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+            completionHandlerForConvertData(nil, NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
+        }
+        
+        completionHandlerForConvertData(parsedResult, nil)
     }
     
         // MARK: Shared Instance
